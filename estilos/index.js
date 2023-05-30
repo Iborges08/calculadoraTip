c397294fe872c6ac8b6e1243d88eb9b689f5d7e8
Iborges08 <iborges081102@gmail.com>
2023-05-30 13:41:56 -0300

    .

--- estilos/index.js
+++ estilos/index.js
@@ -3,4 +3,14 @@
     let tipPercent = document.getElementById ('tipInput').value
     let split = document.getElementById ('splitInput').value
 
+    let tipValue = bill * (tipPercent / 100)
+    let billtotal = bill + tipValue
+    let billEach  = billtotal / split 
+    
+    document.getElementById('tipPercent').innerHTML = tipPercent + ' %'
+    document.getElementById('tipValue').innerHTML = '$ ' + tipValue
+    document.getElementById('totalWithTip').innerHTML = '$ ' + billtotal
+    document.getElementById('splitValue') .innerHTML = split
+    document.getElementById('billEach').innerHTML = billtotal / split 
+    
  }
