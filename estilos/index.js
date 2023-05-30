de813188c91cb066d56dd2a636222bbb6d334302
Iborges08 <iborges081102@gmail.com>
2023-05-30 13:47:35 -0300

    .

--- estilos/index.js
+++ estilos/index.js
@@ -1,4 +1,9 @@
- function update() {
+ function formatMoney(value) {
+   value = value.toFixed(2)
+   return '$ ' + value 
+ }
+
+function update() {
     let bill = Number (document.getElementById('yourBill').value)
     let tipPercent = document.getElementById ('tipInput').value
     let split = document.getElementById ('splitInput').value
@@ -8,9 +13,9 @@
     let billEach  = billtotal / split 
     
     document.getElementById('tipPercent').innerHTML = tipPercent + ' %'
-    document.getElementById('tipValue').innerHTML = '$ ' + tipValue
-    document.getElementById('totalWithTip').innerHTML = '$ ' + billtotal
+    document.getElementById('tipValue').innerHTML = formatMoney(tipValue)
+    document.getElementById('totalWithTip').innerHTML = formatMoney(billtotal)
     document.getElementById('splitValue') .innerHTML = split
-    document.getElementById('billEach').innerHTML = billtotal / split 
+    document.getElementById('billEach').innerHTML = formatMoney (billtotal / split )
     
  }
